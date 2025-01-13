557d1c632690c4c75f8b0712db14001bf819b2c5
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2025-01-13 23:43:27 +0500

    lesson02 about some termenologies

--- Lesson01/build/js/main.js
+++ Lesson01/build/js/main.js
@@ -1,6 +1,20 @@
 "use strict";
-let userName = "Hamid iqbal here";
-console.log(userName);
-let a = 12;
-let b = "4";
-let c = 5;
+// let userName = "Hamid iqbal here"
+// console.log(userName)
+// let a: number = 12;
+// let b:string ="4"
+// let c:number =5;
+// // JavasScript used dyanimc types and therefore it coersed the data becasue it usedd the dynamic type instead of static type.
+// console.log(a/b)
+//Lesson 02
+let myName = "Hamid";
+let meaningOfLife;
+let isLoading;
+let album; // Union type. this can me more than one data type.
+myName = "Iqbal";
+meaningOfLife = 42;
+isLoading = true;
+album = 1982;
+const sum = (a, b) => {
+    return a + b;
+};
--- Lesson01/src/main.ts
+++ Lesson01/src/main.ts
@@ -1,7 +1,26 @@
-let userName = "Hamid iqbal here"
-console.log(userName)
+// let userName = "Hamid iqbal here"
+// console.log(userName)
 
-let a= 12;
-let b ="4"
-let c=5;
+// let a: number = 12;
+// let b:string ="4"
+// let c:number =5;
+
+// // JavasScript used dyanimc types and therefore it coersed the data becasue it usedd the dynamic type instead of static type.
+
+// console.log(a/b)
+
+//Lesson 02
+let myName:string = "Hamid";
+let meaningOfLife:number;
+let isLoading:boolean;
+let album:number | string;// Union type. this can me more than one data type.
+
+myName="Iqbal";
+meaningOfLife=42;
+isLoading=true;
+album=1982
+
+const sum =(a:number,b:number)=>{
+    return a+b;
+}
 
